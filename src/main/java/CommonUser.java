7b447b8b511e461f233216334925f33e47ba1b04
ma-za-kpe <makpalyy@gmail.com>
2019-05-01 20:21:53 +0200

    added all commonUser tests tat are passing

--- src/main/java/CommonUser.java
+++ src/main/java/CommonUser.java
@@ -1,3 +1,92 @@
-public class CommonUser {
+import org.sql2o.Connection;
 
+import java.util.List;
+
+public class CommonUser extends Users {
+
+    //CONSTANTS
+    public static final String ROLE = "commonUser";
+
+    public CommonUser(String name, String role) {
+        this.name = name;
+        this.role = ROLE;
+    }
+
+    @Override
+    public int getId() {
+        return super.getId();
+    }
+
+    @Override
+    public String getName() {
+        return super.getName();
+    }
+
+    @Override
+    public String getRole() {
+        return super.getRole();
+    }
+
+    @Override
+    public boolean equals(Object otherCommonUser){
+        if (!(otherCommonUser instanceof CommonUser)) {
+            return false;
+        } else {
+            CommonUser newCommonUser= (CommonUser) otherCommonUser;
+            return this.getName().equals(newCommonUser.getName());
+        }
+    }
+
+    //save CommonUser into db
+    public void save() {
+        try (Connection con = DB.sql2o.open()) {
+            String sql = "INSERT INTO users (name, role) VALUES (:name, :role);";
+            this.id = (int) con.createQuery(sql, true)
+                    .addParameter("name", this.name)
+                    .addParameter("role", this.role)
+                    .executeUpdate()
+                    .getKey();
+        }
+    }
+
+    //get all CommonUsers
+    public static List<CommonUser> all() {
+        String sql = "SELECT * FROM users WHERE role='commonUser';";
+        try(Connection con = DB.sql2o.open()) {
+            return con.createQuery(sql).executeAndFetch(CommonUser.class);
+        }
+    }
+
+    //find animals by id
+    public static CommonUser find(int id) {
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "SELECT * FROM users where id=:id";
+            CommonUser CommonUser = con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeAndFetchFirst(CommonUser.class);
+            return CommonUser;
+        }
+    }
+    //
+    //update an CommonUser
+    public void update(String name) {
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "UPDATE users SET name = :name WHERE id = :id";
+            con.createQuery(sql)
+                    .addParameter("name", name)
+                    .addParameter("id", id)
+                    .executeUpdate();
+        }
+    }
+
+    //delete CommonUser
+    public void delete() {
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "DELETE FROM users WHERE id = :id;";
+            con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeUpdate();
+        }
+    }
+    
 }
--- src/test/java/CommonUserTest.java
+++ src/test/java/CommonUserTest.java
@@ -1,5 +1,95 @@
+import org.junit.Rule;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class CommonUserTest {
+
+    @Rule
+    public DatabaseRule database = new DatabaseRule();
+
+    @Test
+    public void CommonUser_instantiatesCorrectly_true() {
+        CommonUser CommonUser= new CommonUser("lion",  "commonUser");
+        assertEquals(true, CommonUser instanceof CommonUser);
+    }
+
+    @Test
+    public void CommonUserInstantiatesWithName_true() throws Exception {
+        CommonUser CommonUser = new CommonUser("lion", "CommonUser");
+        assertEquals("lion", CommonUser.getName());
+
+    }
+
+    @Test
+    public void CommonUserInstantiatesWithRole_true() throws Exception {
+        CommonUser CommonUser = new CommonUser("lion",  "commonUser");
+        assertEquals("commonUser", CommonUser.getRole());
+
+    }
+
+
+    @Test
+    public void equals_returnsTrueIfNameAreSame_true() {
+        CommonUser CommonUser = new CommonUser("lion",  "commonUser");
+        CommonUser CommonUser1 = new CommonUser("lion",  "commonUser");
+        assertTrue(CommonUser.equals(CommonUser1));
+    }
+
+    @Test
+    public void save_insertsObjectIntoDatabase_kitenge() {
+        CommonUser testCommonUser= new CommonUser("lion",  "commonUser");
+        testCommonUser.save();
+        assertTrue(CommonUser.all().get(0).equals(testCommonUser));
+    }
+
+    //return all instances of CommonUser
+    @Test
+    public void all_returnsAllInstancesOfCommonUser_true() {
+        CommonUser firstCommonUser = new CommonUser("lion",  "commonUser");
+        firstCommonUser.save();
+        CommonUser secondCommonUser = new CommonUser("kangaroo", "commonUser");
+        secondCommonUser.save();
+        assertEquals(true, CommonUser.all().get(0).equals(firstCommonUser));
+        assertEquals(true, CommonUser.all().get(1).equals(secondCommonUser));
+    }
+
+    //saving our ids form the db to our classes
+    @Test
+    public void save_assignsIdToObject() {
+        CommonUser testCommonUser= new CommonUser("lion", "commonUser");
+        testCommonUser.save();
+        CommonUser savedCommonUser = CommonUser.all().get(0);
+        assertEquals(testCommonUser.getId(), savedCommonUser.getId());
+    }
+
+    //find CommonUser based on their id
+    @Test
+    public void find_returnsCommonUserWithSameId_secondCommonUser() {
+        CommonUser firstCommonUser= new CommonUser("camel",  "commonUser");
+        firstCommonUser.save();
+        CommonUser secondCommonUser = new CommonUser("wathog",  "commonUser");
+        secondCommonUser.save();
+        assertEquals(CommonUser.find(secondCommonUser.getId()), secondCommonUser);
+    }
+
+    //update CommonUser
+    @Test
+    public void update_updatesCommonUserName_true() {
+        CommonUser myCommonUser= new CommonUser("lion",  "commonUser");
+        myCommonUser.save();
+        myCommonUser.update("elephant");
+        assertEquals("elephant", CommonUser.find(myCommonUser.getId()).getName());
+    }
+
+    @Test
+    public void delete_deletesCommonUser_true() {
+        CommonUser myCommonUser = new CommonUser("lion", "commonUser");
+        myCommonUser.save();
+        int myCommonUserId = myCommonUser.getId();
+        myCommonUser.delete();
+        assertEquals(null, CommonUser.find(myCommonUserId));
+    }
+
 
 }
