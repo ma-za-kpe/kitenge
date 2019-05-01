8d2ea074023f0913d9174d36b7b8b9b4f69320d2
ma-za-kpe <makpalyy@gmail.com>
2019-05-01 19:46:30 +0200

    added save method in vendor.java class

--- src/main/java/Vendor.java
+++ src/main/java/Vendor.java
@@ -1,3 +1,5 @@
+import org.sql2o.Connection;
+
 public class Vendor extends Users {
 
     public String productCategory;
@@ -27,5 +29,29 @@ public class Vendor extends Users {
     @Override
     public String getRole() {
         return super.getRole();
+    }
+
+    @Override
+    public boolean equals(Object otherVendor){
+        if (!(otherVendor instanceof Vendor)) {
+            return false;
+        } else {
+            Vendor newVendor= (Vendor) otherVendor;
+            return this.getName().equals(newVendor.getName());
+        }
+    }
+
+    //save vendor into db
+    public void save() {
+        try (Connection con = DB.sql2o.open()) {
+            String sql = "INSERT INTO animals (name, type, sightingid, daterecorded) VALUES (:name, :type, :sightingId, now());";
+            this.id = (int) con.createQuery(sql, true)
+                    .addParameter("name", this.name)
+                    .addParameter("type", this.type)
+                    .addParameter("sightingId", this.sightingId)
+                    .addParameter("dateRecorded", this.dateRecorded)
+                    .executeUpdate()
+                    .getKey();
+        }
     }
 }
