98e7ebddc50f508e71fd82cb746cb92df3f08d8a
ma-za-kpe <makpalyy@gmail.com>
2019-05-03 08:14:53 +0200

    added the heroku postgresql db link to the DB.java file

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,6 +1,7 @@
 import org.sql2o.Sql2o;
 
 public class DB {
-    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/kitenge", "maku", "maku");
-//    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-54-225-116-36.compute-1.amazonaws.com:5432/da9aaps8f9r9sa", "glwcnxtcekkthr", "85db316d6c524a41cff623d72318efa5b1802320fcf82d58082d7f55711838a8");
+//    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/kitenge", "maku", "maku");
+    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-23-23-228-132.compute-1.amazonaws.com:5432/d4fr3strkacg8f", "slwawxuxvvowrh", "3c1e3fd6fc200014f3757f48f4a04408ecb2c4cae483537ffaf516881ecf53b2");
+
 }
