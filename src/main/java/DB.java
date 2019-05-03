1edf123e5c7c1a5f0078917ae80d405ac9d30c9d
ma-za-kpe <makpalyy@gmail.com>
2019-05-03 10:19:54 +0200

    modified the DB.java file

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,7 +1,7 @@
 import org.sql2o.Sql2o;
 
 public class DB {
-//    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/kitenge", "maku", "maku");
-    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-23-23-228-132.compute-1.amazonaws.com:5432/d4fr3strkacg8f", "slwawxuxvvowrh", "3c1e3fd6fc200014f3757f48f4a04408ecb2c4cae483537ffaf516881ecf53b2");
+    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/kitenge", "maku", "maku");
+//    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-23-23-228-132.compute-1.amazonaws.com:5432/d4fr3strkacg8f", "slwawxuxvvowrh", "3c1e3fd6fc200014f3757f48f4a04408ecb2c4cae483537ffaf516881ecf53b2");
 
 }
